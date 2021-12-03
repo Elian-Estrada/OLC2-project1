5f5c242bdbfb7f1dcf0d3a3a022593a99ef420f9
ElianEstrada <elian_estrada@debian-BULLSEYE-live-builder-AMD64>
2021-12-02 19:36:38 -0600

    first commit

---
+++ src/Main.js
@@ -0,0 +1,9 @@
+var Main = /** @class */ (function () {
+    function Main() {
+    }
+    Main.prototype.lexicalAnalysis = function (bufferStream) {
+        console.log("Analizando ".concat(bufferStream));
+    };
+    return Main;
+}());
+export { Main };
