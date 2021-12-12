f14cab972ef5aacc02b7975c7c9c8571a4900235
JD <JDVelasquezO>
2021-12-12 12:09:29 -0600

    Substring maked

--- dist/src/Nativas/SubString.js
+++ dist/src/Nativas/SubString.js
@@ -1,1 +1,39 @@
-"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+import { Function } from "../Instructions/Function.js";
+import Exception from "../SymbolTable/Exception.js";
+import { type } from "../SymbolTable/Type.js";
+var SubString = /** @class */ (function (_super) {
+    __extends(SubString, _super);
+    function SubString(id, from, to, type, name, params, instructions, row, col) {
+        var _this = _super.call(this, type, name, params, instructions, row, col) || this;
+        _this.id = id;
+        _this.from = from;
+        _this.to = to;
+        return _this;
+    }
+    SubString.prototype.interpret = function (tree, table) {
+        var id_founded = this.id.interpret(tree, table);
+        if (id_founded === null)
+            return new Exception("Semantic", "Identifier not found in the current context", this.row, this.column);
+        if (this.id.get_type() !== type.STRING)
+            return new Exception("Semantic", "The type ".concat(id_founded.type, " not valid for Length"), this.row, this.column);
+        this.type = type.STRING;
+        return id_founded.substring(this.from, this.to);
+    };
+    return SubString;
+}(Function));
+export { SubString };
