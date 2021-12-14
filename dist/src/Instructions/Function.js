77ed7e50ea34ed612c1c52af83c7b2a297b91dc6
ElianEstrada <chictibiris@gmail.com>
2021-12-13 23:59:25 -0600

    fixed function again

--- dist/src/Instructions/Function.js
+++ dist/src/Instructions/Function.js
@@ -20,7 +20,6 @@ import { Break } from "./Break.js";
 import { Return } from "./Return.js";
 import { Continue } from "./Continue.js";
 import { type } from "../SymbolTable/Type.js";
-import { If } from "./If.js";
 var Function = /** @class */ (function (_super) {
     __extends(Function, _super);
     function Function(type, name, params, instructions, row, col) {
@@ -31,84 +30,38 @@ var Function = /** @class */ (function (_super) {
         _this.type = type;
         return _this;
     }
-    Function.prototype.look_for_a_return = function (instruction) {
-        var flag = false;
-        var count = 0;
-        if (instruction instanceof If) {
-            for (var _i = 0, _a = instruction.instructions; _i < _a.length; _i++) {
-                var instr = _a[_i];
-                if (flag)
-                    return flag;
-                // console.log(instr);
-                if (instr instanceof Return) {
-                    console.log("Hola");
-                    flag = true;
-                    break;
-                }
-                else {
-                    this.look_for_a_return(instr);
-                }
-            }
-        }
-        else {
-            count += 1;
-            this.look_for_a_return(this.instructions[count]);
-        }
-        return flag;
-    };
     Function.prototype.interpret = function (tree, table) {
         var new_table = new SymbolTable(table, "Function-".concat(this.name, "-").concat(this.row, "-").concat(this.column));
-        var flag = false;
-        if (this.type == type.VOID)
-            flag = true;
-        if (!flag) {
-            for (var _i = 0, _a = this.instructions; _i < _a.length; _i++) {
-                var instr = _a[_i];
-                if (instr instanceof Return) {
-                    flag = true;
-                    break;
-                }
-                else {
-                    this.look_for_a_return(instr);
-                }
+        for (var _i = 0, _a = this.instructions; _i < _a.length; _i++) {
+            var instruction = _a[_i];
+            var value = instruction.interpret(tree, new_table);
+            if (value === "void")
+                return;
+            if (value instanceof Exception) {
+                tree.get_errors().push(value);
+                tree.update_console(value.toString());
             }
-        }
-        for (var _b = 0, _c = this.instructions; _b < _c.length; _b++) {
-            var instruction = _c[_b];
-            // console.log(instruction)
-            if (flag) {
-                var value = instruction.interpret(tree, new_table);
-                if (value === "void")
-                    return;
-                if (value instanceof Exception) {
-                    tree.get_errors().push(value);
-                    tree.update_console(value.toString());
-                }
-                var error = null;
-                if (value instanceof Break) {
-                    error = new Exception("Semantic", "Instruction Break out of loop", instruction.row, instruction.column);
-                    tree.get_errors().push(error);
-                    // tree.get_update(error);
-                }
-                if (value instanceof Continue) {
+            var error = null;
+            if (value instanceof Break) {
+                error = new Exception("Semantic", "Instruction Break out of loop", instruction.row, instruction.column);
+                tree.get_errors().push(error);
+                // tree.get_update(error);
+            }
+            if (value instanceof Continue) {
+                // console.log("Hola")
+                error = new Exception("Semantic", "Instruction Continue out of loop", instruction.row, instruction.column);
+                tree.get_errors().push(error);
+            }
+            if (value instanceof Return) {
+                console.log(this.type);
+                if (this.type == type.VOID) {
                     // console.log("Hola")
-                    error = new Exception("Semantic", "Instruction Continue out of loop", instruction.row, instruction.column);
-                    tree.get_errors().push(error);
+                    return new Exception("Semantic", "Function should not return anything", instruction.row, instruction.column);
                 }
-                if (value instanceof Return) {
-                    console.log(this.type);
-                    if (this.type == type.VOID) {
-                        // console.log("Hola")
-                        return new Exception("Semantic", "Function should not return anything", instruction.row, instruction.column);
-                    }
-                    if (this.type != value.get_type()) {
-                        return new Exception("Semantic", "Function doesn't return same data type", instruction.row, instruction.column);
-                    }
-                    return value.get_result();
+                if (this.type != value.get_type()) {
+                    return new Exception("Semantic", "Function doesn't return same data type", instruction.row, instruction.column);
                 }
-            }
-            else {
-                return new Exception("Semantic", "Function doesn't return anything", instruction.row, instruction.column);
+                return value.get_result();
             }
         }
         if (this.type !== type.VOID) {
