1a53291babbf9256afb1264174e0e894fdc5d9fe
ElianEstrada <chictibiris@gmail.com>
2021-12-18 19:57:40 -0600

    fixed

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ myCodeMirror = CodeMirror.fromTextArea(
     }
 )
 
-myCodeMirror.setValue("void main(){\n\n  return;\n}");
+myCodeMirror.setValue("void main(){\n  return;\n}");
 
 // @ts-ignore
 let myCodeMirror2 = CodeMirror.fromTextArea(
